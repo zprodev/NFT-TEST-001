61f4c81a9beb000cdebbae3d2271e543cc6bb14a
zprodev <zproject.develop@gmail.com>
2022-12-02 20:36:41 +0900

    return json in tokenURI

--- contracts/ZprodevNftTest001.sol
+++ contracts/ZprodevNftTest001.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract ZprodevNftTest001 is ERC721 {
     using Counters for Counters.Counter;
+    using Strings for uint256;
 
     Counters.Counter private _tokenIdCounter;
 
@@ -15,5 +16,26 @@ contract ZprodevNftTest001 is ERC721 {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        pure
+        override(ERC721)
+        returns (string memory)
+    {
+        bytes memory bytesName = abi.encodePacked(
+            '"name":"TEST #', tokenId.toString(), '"'
+        );
+        bytes memory bytesImage = abi.encodePacked(
+            '"image":"https://via.placeholder.com/256.png/FFFFFF/000000/?text=', tokenId.toString(), '"'
+        );
+        bytes memory bytesJson = abi.encodePacked(
+            '{',
+                bytesName, ',',
+                bytesImage,
+            '}'
+        );
+        return string(bytesJson);
     }
 }
--- test/zprodev_nft_test001.js
+++ test/zprodev_nft_test001.js
@@ -14,5 +14,16 @@ contract("ZprodevNftTest001", function (accounts) {
     await instance.safeMint(accounts[1]);
     const afterBalance = await instance.balanceOf(account);
     assert.isTrue(afterBalance.toString() == '1');
+    const hoge = await instance.tokenURI(1);
+    console.log(hoge);
+
+  });
+
+  it("should return json in tokenURI", async function () {
+    const instance = await ZprodevNftTest001.deployed();
+    const token1 = await instance.tokenURI(1);
+    assert.isTrue(token1.toString() == '{"name":"TEST #1","image":"https://via.placeholder.com/256.png/FFFFFF/000000/?text=1"}');
+    const token2 = await instance.tokenURI(2);
+    assert.isTrue(token2.toString() == '{"name":"TEST #2","image":"https://via.placeholder.com/256.png/FFFFFF/000000/?text=2"}');
   });
 });
